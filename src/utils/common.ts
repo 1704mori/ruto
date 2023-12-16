ffdd1698518bae9b6784eb006a032a09aa68677c
mori1704 <1704mori@gmail.com>
2023-12-16 10:10:31 -0300

    Make readRoutesFolder root parameter required

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,31 +1,24 @@
 import path from "node:path";
 import fs from "node:fs/promises";
-import { routesPath as _routesPath } from "../ruto";
-
-let routesPath = _routesPath;
 
 export const METHODS = ["get", "post", "put", "delete"] as const;
 
 export async function readRoutesFolder(
-  root?: string,
+  root: string,
 ): Promise<[string[] | null, string | null]> {
-  if (root) {
-    routesPath = path.join(root);
+  console.log("[ruto]: reading routes from %s", root);
+
+  if (root.endsWith(".ts") || root.endsWith(".js")) {
+    root = root.split("/").slice(0, -1).join("/");
   }
 
-  console.log("[ruto]: reading routes from %s", routesPath);
-
-  if (routesPath.endsWith(".ts") || routesPath.endsWith(".js")) {
-    routesPath = routesPath.split("/").slice(0, -1).join("/");
-  }
-
-  if (!(await fs.stat(routesPath))) {
+  if (!(await fs.stat(root))) {
     return [null, "routes folder not found"];
   }
 
   const routes: any[] = [];
 
-  for (const route of await fs.readdir(routesPath)) {
+  for (const route of await fs.readdir(root)) {
     // if ((await fs.stat(route)).isDirectory()) {
     //   continue;
     // }
